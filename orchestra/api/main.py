8f455182ec2a58afd42f1091685251e8746fd47d
András Vidosits <vidosits.andras@gmail.com>
2024-03-07 16:39:41 +0100

    Add /tags endpoint for querying tags

--- orchestra/api/main.py
+++ orchestra/api/main.py
@@ -5,7 +5,7 @@ import signal
 from fastapi import FastAPI
 from rich.logging import RichHandler
 
-from orchestra.api.routers import jobs, tasks
+from orchestra.api.routers import jobs, tasks, tags
 from orchestra.core import instance
 
 logging.basicConfig(
@@ -17,6 +17,7 @@ logger = logging.getLogger("orchestra.api")
 app = FastAPI(title="Orchestra")
 app.include_router(jobs.router)
 app.include_router(tasks.router)
+app.include_router(tags.router)
 
 if instance is None:
     logger.error("Orchestra is not started, can't start API server")
---
+++ orchestra/api/routers/tags.py
@@ -0,0 +1,14 @@
+from typing import Annotated
+
+from fastapi import APIRouter, Query
+from orchestra.core import instance
+
+router = APIRouter()
+
+
+@router.get("/tags/", tags=["tags"])
+async def get_all_tags(include_paused: Annotated[bool, Query(description="Set to true to include paused jobs when collecting tags")] = True) -> set[str]:
+    collected_tags: set[str] = set()
+    for job in instance.get_jobs(set(), any_tag=True, include_paused=include_paused):
+        collected_tags.update(job.tags)
+    return collected_tags
